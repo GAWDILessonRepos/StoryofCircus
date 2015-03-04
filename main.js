3786b49f848f5ac7ad96c4e0f40e006e03effe93
pat <patrick.h.needham@gmail.com>
2015-03-04 14:24:10 -0500

    jquery buttons for bg color to blue, text to red, li for each input val

--- main.js
+++ main.js
@@ -1,1 +1,19 @@
-a
+var Silly = Silly || {}
+$bg_button = $('#change_color');
+
+$(document).ready(function(){
+  $bg_button.on('click', function(){
+    $('body').css("background-color", "blue");
+  });
+  $('#change_text').on('click', function(){
+    $('body').css("color", "red");
+  });
+
+  $('#myForm').on('submit', function(){
+    $('#myInput').append($('<li>').text($('myInput').val()));
+  });
+
+});
+
+
+
